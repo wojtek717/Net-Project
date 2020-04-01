0e2884a7c731fc9978298972d0fbdd405de377fe
Student 241488 <241488@student.pwr.edu.pl>
2020-04-01 20:35:24 +0200

    Connect to new API and get JSON

--- CityWeather/ApiService.cs
+++ CityWeather/ApiService.cs
@@ -5,16 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace CityWeather
 {
     class ApiService
     {
-        string sAttr;
+        string apiKey;
+        string apiBaseUrl;
+
+        string countryShort = "PL";
+
+        static HttpClient client = new HttpClient();
 
         public ApiService()
         {
-            sAttr = ConfigurationManager.AppSettings.Get("APIKEY");
+            apiKey = ConfigurationManager.AppSettings.Get("APIKEY");
+            apiBaseUrl = ConfigurationManager.AppSettings.Get("APIURL");
+
+            getCityJson("Poznan", 3);
         }
+
+
+        async void getCityJson(string city, int days) {
+            var json = await client.GetStringAsync(apiBaseUrl + city + "," + countryShort + "&key=" + apiKey + "&days=" + (days.ToString()));
+            Console.WriteLine(json);
+        }
+
+
+
+
+
+
     }
 }
